361f507ad79f94d0637aadf6893e4136b185f110
Humphrey Murray <humphrey@datalive.com.au>
2021-02-16 11:43:15 +1100

    Quick hack

---
+++ README.md
@@ -0,0 +1,7 @@
+A quick hacky screen recorder.
+
+```shell
+./record.py . --region=0,0,4000,2550
+```
+
+Screenshots will be grouped into folders by date
---
+++ record.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import time
+import subprocess
+from pathlib import Path, PurePath
+import argparse
+import sys
+
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('basedir', type=PurePath)
+parser.add_argument('--region', nargs="?", type=str, help='X,Y,W,H')
+args = parser.parse_args()
+
+
+started_at = time.localtime()
+directory = PurePath.joinpath(args.basedir, time.strftime("%Y-%m-%d"))
+prefix = f'{time.strftime("%Y%m%d")}'
+
+Path(directory).mkdir(parents=True, exist_ok=True)
+
+
+region = args.region or '0,0,4000,2550'
+
+# prev_mins = None
+
+while True:
+
+  minutes = time.localtime().tm_hour * 60 +  time.localtime().tm_min
+  output_file = f'{directory}/{prefix}_{str(minutes).rjust(5, "0")}.jpg'
+
+  if Path(output_file).exists():
+    time.sleep(30)
+    continue
+    
+  cmd = [
+    'screencapture',              # MacOS provided command which takes screenshots
+    '-x',                         # Do not make sound
+    '-R', region,                 # Capture my entire multi-screen area.  This also downscales the retina screen
+    '-t', 'jpg',                  # JPG to make files smaller
+    f'{directory}/{prefix}_{str(minutes).rjust(5, "0")}.jpg',     # Filename
+  ]
+  print(' '.join(cmd))
+  subprocess.run(cmd)
